f48a2bffea1fa853be8f5c496f0c8342c4a0f990
Ikhramsyamsuri <muhammaikhram3@gmail.com>
2023-12-12 21:07:36 +0700

    Delete api Folder

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Project Name 
 ## Introduction 
 ## Installation
+python
+ubunttu
+sudo apt install python3-pip python3-venv
+django sudo apt install python3-django
+rest framework
+mysql
+
 ## Model Explanition
 ## Function Explanition
 ## Folder Struktur
--- store/urls.py
+++ store/urls.py
@@ -1,22 +1,13 @@
-"""
-URL configuration for store project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('api.urls'))
+    path('', views.landingpage),
+    path('product/', views.renderhtml, name= 'produk'),
+    path('product/add/', views.add, name= 'add'),
+    path('product/add/addrecord/', views.addrecord, name= 'addproduk'),
+    path('product/delete/<int:id>', views.delete, name='delete'),
+    path('product/edit/<int:id>', views.edit, name='edit'),
+    path('product/edit/edited/<int:id>', views.edited, name='edited'),
+
 ]
